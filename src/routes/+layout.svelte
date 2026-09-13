f067ee5107c89c1421efd752f14b6a2dbebc60bc
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-09-13 18:57:29 -0500

    Add routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+import favicon from '$lib/assets/favicon.png'
 
-	let { children } = $props();
+let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
+    <meta
+        name="description"
+        content="Klar is a progressive programming language designed for everyone. Simple, safe, and clean."
+    />
+    <meta
+        name="keywords"
+        content="javascript, klar, docs, programming language, playground, tour"
+    />
 </svelte:head>
 
 {@render children()}
